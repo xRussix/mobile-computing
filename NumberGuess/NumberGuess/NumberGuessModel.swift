e0fa0d13d4e51ff323878dbb56109cc5f12e702a
Simon Ruspekhofer <simonrusp@gmail.com>
2021-11-18 13:27:52 +0100

    commited new version

---
+++ NumberGuess/NumberGuess/NumberGuessModel.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+class NumberGuessModel {
+    
+    var target = 0
+    var guessCount = 0
+    
+    func compare(to: Int) -> Int {
+        return target - to
+    }
+    
+    func isValid(string: String?) -> Bool {
+        var isValid = false
+        if let str = string, let guess = Int(str) {
+            if guess >= 1 && guess <= 99 {
+                isValid = true
+            }
+        }
+        return isValid
+    }
+    
+    func getImage() -> String {
+        var imgName: String = ""
+        if (guessCount < 6) {
+            imgName = "happy"
+        } else if (guessCount < 10) {
+            imgName = "neutral"
+        } else {
+            imgName = "sad"
+        }
+        return imgName
+    }
+}
